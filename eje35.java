5c5279892dc1f56069e21bcd3996ef1e427f99b7
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-10-05 12:45:32 +0200

    acabado eje35

---
+++ eje35.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+public class eje35{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca la altura de la X ");
+        int altura= sc.nextInt();
+        int j=0;
+        int g=0;
+
+        for(int i=1;i<=altura;i++){
+
+            while(j>0){
+                System.out.print(" ");
+                j--;
+            }
+            System.out.print("*");
+
+            for(int k=1;k<=altura-i;k++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<altura-i;k++){
+                System.out.print(" ");
+            }
+            if(altura!=i){
+            System.out.println("*");
+            }
+            j=i;
+
+        }
+
+    //    System.out.println(" ");
+
+        for(int i=1;i<=altura;i++){
+
+            while(g<altura-2){
+                System.out.print(" ");
+                g++;
+            }
+            if(i!=1){
+            System.out.print("*");
+            }
+
+            for(int k=altura;k>altura-i+2;k--){
+                System.out.print(" ");
+            }
+
+            for(int k=altura;k>=altura-i+2;k--){
+                System.out.print(" ");
+            }
+            
+            if(i!=1){
+            System.out.print("*");
+            }
+            g=i-1;
+            System.out.println("");
+        }
+
+
+        sc.close();
+        }
+}
