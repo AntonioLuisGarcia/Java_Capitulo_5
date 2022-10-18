8df4b91026ea7ef91be7318dd16e47f17609c781
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-10-18 21:17:31 +0200

    ejercicio 21 acabado

--- eje21.java
+++ eje21.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 public class eje21 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Este programa va pidiendo números hasta que se introduzce un numero negativo y te dira cuantos números se han introducido, la media de los impares y el mayor de los pares");
+        System.out.println("Este programa va pidiendo números hasta que se introduzce un numero negativo y te dira cuantos números se han introducido, la media de los impares y el mayor de los pares");
         System.out.print("Introduce un numero");
         boolean salir= true;
         int contador=0;
         int total_impar=0;
         int cont_impar=0;
+        int par_mayor=0;
         while(salir){
             int numero=sc.nextInt();
         if(numero<0){
@@ -18,13 +19,14 @@ public class eje21 {
             total_impar=total_impar+numero;
             cont_impar++;
         }
-        if(numero%2==0){
-            
+        if(numero%2==0 && numero>par_mayor){
+            par_mayor=numero;
         }
         contador++;
     }
-    System.out.print("Se han introducido "+contador+" numeros");
-    System.out.print("La media de los impares es "+total_impar/cont_impar);
+    System.out.println("Se han introducido "+contador+" numeros");
+    System.out.println("La media de los impares es "+total_impar/cont_impar);
+    System.out.println("El mayor numero par es "+par_mayor);
         
     sc.close();
 }
