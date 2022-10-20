1c653ce45848ae3969572808d5fb418999759b0a
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-10-20 21:37:50 +0200

    48 acabado 49 a medias

---
+++ eje48.java
@@ -0,0 +1,126 @@
+// Ejercicio 48
+// Realiza un programa que diga los dígitos que aparecen y los que no aparecen
+// en un número entero introducido por teclado. El orden es el que se muestra en
+// los ejemplos. Utiliza el tipo long para que el usuario pueda introducir números
+// largos.
+
+import java.util.Scanner;
+public class eje48{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Con este programa te diraa que cifras no estan en tu numero ");
+        System.out.println("Diga el numero ");
+        long numero = sc.nextLong();
+        boolean cero=true;
+        boolean uno=true;
+        boolean dos=true;
+        boolean tres=true;
+        boolean cuatro=true;
+        boolean cinco=true;
+        boolean seis=true;
+        boolean siete=true;
+        boolean ocho=true;
+        boolean nueve=true;
+        
+        while(numero>0){
+            if(numero%10==0){
+                cero=false;
+            }
+            if(numero%10==1){
+                uno=false;
+            }
+            if(numero%10==2){
+                dos=false;
+            }
+            if(numero%10==3){
+                tres=false;
+            }
+            if(numero%10==4){
+                cuatro=false;
+            }
+            if(numero%10==5){
+                cinco=false;
+            }
+            if(numero%10==6){
+                seis=false;
+            }
+            if(numero%10==7){
+                siete=false;
+            }
+            if(numero%10==8){
+                ocho=false;
+            }
+            if(numero%10==9){
+                nueve=false;
+            }
+            numero/=10;
+        }
+        
+        System.out.print("Los numeros que aparecen son:");
+        if(cero==false){
+            System.out.print(" 0 ");
+        }
+        if(uno==false){
+            System.out.print(" 1 ");
+        }
+        if(dos==false){
+            System.out.print(" 2 ");
+        }
+        if(tres==false){
+            System.out.print(" 3 ");
+        }
+        if(cuatro==false){
+            System.out.print(" 4 ");
+        }
+        if(cinco==false){
+            System.out.print(" 5 ");
+        }
+        if(seis==false){
+            System.out.print(" 6 ");
+        }
+        if(siete==false){
+            System.out.print(" 7 ");
+        }
+        if(ocho==false){
+            System.out.print(" 8 ");
+        }
+        if(nueve==false){
+            System.out.print(" 9 ");
+        }
+
+        System.out.println("");
+        System.out.print("Los numeros que no aparecen son:");
+        if(cero){
+            System.out.print(" 0 ");
+        }
+        if(uno){
+            System.out.print(" 1 ");
+        }
+        if(dos){
+            System.out.print(" 2 ");
+        }
+        if(tres){
+            System.out.print(" 3 ");
+        }
+        if(cuatro){
+            System.out.print(" 4 ");
+        }
+        if(cinco){
+            System.out.print(" 5 ");
+        }
+        if(seis){
+            System.out.print(" 6 ");
+        }
+        if(siete){
+            System.out.print(" 7 ");
+        }
+        if(ocho){
+            System.out.print(" 8 ");
+        }
+        if(nueve){
+            System.out.print(" 9 ");
+        }
+      
+        sc.close();
+        }
+	}
---
+++ eje49.java
@@ -0,0 +1,50 @@
+// Ejercicio 49
+// Realiza un programa que calcule el máximo, el mínimo y la media de una serie
+// de números enteros positivos introducidos por teclado. El programa terminará
+// cuando el usuario introduzca un número primo. Este último número no se
+// tendrá en cuenta en los cálculos. El programa debe indicar también cuántos
+// números ha introducido el usuario (sin contar el primo que sirve para salir).
+
+import java.util.Scanner;
+public class eje49{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce numeros, se acaba cuando introduzcas un numero primo ");
+        
+        boolean noprimo=true;
+        int contador=0;
+        int total=0;
+        int max=0;
+        int min=1264983232;
+        
+        while(noprimo){
+
+            System.out.println("Diga un numero ");
+            int numero=sc.nextInt();
+
+            for(int i=2;i<numero;i++){
+                if(numero%i!=0){
+                    noprimo=false;
+                    break;
+                }
+            }
+            if(noprimo){
+                System.out.println(numero);
+                contador++;
+                total=total+numero;
+                if(numero>max){
+                    max=numero;
+                }
+                if(numero<min){
+                    min=numero;
+                }
+            }
+        }
+        System.out.print("Has metido "+contador+" numeros primos");
+        System.out.print("La media es "+total/contador);
+        System.out.print("El numero maximo es "+max);
+        System.out.print("El numero minimo es "+min);
+
+        sc.close();
+        }
+	}
