285f15d3f7fb9b73aa82c75e6519c44b53fdf279
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-10-22 19:25:38 +0200

    ejercicios del 64 al 69 hechos

---
+++ eje66.java
@@ -0,0 +1,47 @@
+// Ejercicio 66
+// La Guardia Civil de Tráfico nos ha encargado un programa que pinte una señal
+// para desviar el tráfico hacia la derecha. La señal es una doble flecha con el
+// vértice apuntando a la derecha. Se pide al usuario la altura de la figura, que
+// debe ser un número impar mayor o igual que 3. La distancia entre cada flecha
+// de asteriscos es siempre de 4 espacios. Si la altura introducida por el usuario
+// no es un número impar mayor o igual que 3, el programa debe mostrar un
+// mensaje de error.
+
+import java.util.Scanner;
+public class eje66 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("De que altura quieres la A,mayor que 2");
+        int altura = sc.nextInt();
+        int altura_1 = altura-1;
+        System.out.println("Donde quieres el palo de la A,entre el 2 y el "+ altura_1);
+        int altura_palo = sc.nextInt();
+        if(altura<3 && altura_palo>altura-1 && altura_palo<2){
+            for (int i = 1; i <= altura; i++) {
+                for (int j = i; j <altura ; j++){
+                    System.out.print(" "); 
+                }
+                
+                if(i==altura_palo){
+                    for(int j=1;j<=i*2-1;j++){
+                        System.out.print("*");
+                    }
+                }else{
+                    if(i!=1){
+                        System.out.print("*");
+                        }
+                    for (int k = 1; k < (i-1)*2 ; k++){
+                        System.out.print(" "); 
+                    }
+                    if(i!=altura_palo){
+                    System.out.print("*");
+                    }
+                }
+                System.out.println("");
+            }
+        }else{
+            System.out.println("ERROR");
+        }
+        sc.close();
+    }
+}
---
+++ eje68.java
@@ -0,0 +1,28 @@
+// Ejercicio 68
+// Escribe un programa que pida un número por teclado y que luego lo “disloque” de tal forma que a cada dígito se le suma 1 si es par y se le resta 1 si
+// es impar. Usa long en lugar de int donde sea necesario para admitir números
+// largos.
+
+import java.util.Scanner;
+public class eje68 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Di un numero y lo dislocaremos");
+        long numero = sc.nextLong();
+        long volteado=0;
+        System.out.println("El numero dislocado es:");
+        while(numero>0){
+            volteado=(volteado*10)+numero%10;
+            numero/=10;
+        }
+        while(volteado>0){
+            if((volteado%10)%2!=0){
+                System.out.print(volteado%10-1);
+            }else{
+                System.out.print(volteado%10+1);
+            }
+            volteado/=10;
+        }
+        sc.close();
+    }
+}
