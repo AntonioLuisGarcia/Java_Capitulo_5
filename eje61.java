bf02d0f331c3b5256cc9b2ef3a7f3723820a07d8
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-10-22 19:43:18 +0200

    ejercicio 61 acabadp

--- eje61.java
+++ eje61.java
@@ -8,11 +8,24 @@ import java.util.Scanner;
 public class eje61{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-        System.out.println("Con este programa te dire si tu numero es de la suerte o no ");
-        System.out.println("Diga un numero");
-        int numero=sc.nextInt();
-
-       
+        System.out.println("Diga la altura de la V ");
+        int altura=sc.nextInt();
+        for(int i=1;i<=altura;i++){
+            for(int j=1;j<=i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.print("***");
+            for(int j=i*2;j<=altura*2-2;j++){
+                System.out.print(" ");
+            }
+            if(i!=altura){
+            System.out.print(" ***");
+            }else{
+                System.out.print("***");
+            }
+            System.out.println("");
+            
+        }
         sc.close();
         }
 	}
